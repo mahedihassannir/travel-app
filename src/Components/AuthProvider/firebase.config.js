ebadeca482f4cc9042bafc78b9645d5fd56ea139
mahedi6666655555 <mahedinir34678@gmail.com>
2023-04-30 12:57:46 +0600

    banne done

--- src/Components/AuthProvider/firebase.config.js
+++ src/Components/AuthProvider/firebase.config.js
@@ -1,5 +1,21 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtVFo0oK_nsP2Z0iZ96pcC76f0FkX-1NI",
+  authDomain: "travle-app-3f9db.firebaseapp.com",
+  projectId: "travle-app-3f9db",
+  storageBucket: "travle-app-3f9db.appspot.com",
+  messagingSenderId: "853980744611",
+  appId: "1:853980744611:web:2b9cc12f666fe667b7e816"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 export default app
--- src/Components/Pages/Header.jsx
+++ src/Components/Pages/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../Css/Header.css'
 
 
 
@@ -8,8 +9,8 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <nav className='flex justify-between pt-2 items-center  px-20'>
+        <div className='w-full h-screen bg-background '>
+            <nav className=' text-white flex justify-between pt-2 items-center  px-20'>
 
                 <Link to="/">
                     <img className='h-20' src="https://i.ibb.co/JKx1xBm/logo.png" alt="" />
@@ -26,6 +27,18 @@ const Header = () => {
 
                 </ul>
             </nav>
+            {/* here isi banner section */}
+            <div className='flex items-center w-full h-screen'>
+                <div className='w-1/2 text-white ml-10'>
+                    <h1 className='text-6xl font-bold text-white'>Cox's bazar</h1>
+                    <p className='font-bold font-sans w-11/12  py-8'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                    <button className='text-white p-4 px-6 bg-orange-500 rounded-lg'>
+                        Booking
+                    </button>
+
+                </div>
+
+            </div>
         </div>
     );
 };
